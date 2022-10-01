6c840795986e9cff11e363947a47f00157541aa2
lhr20080401 <112790672+lhr20080401@users.noreply.github.com>
2022-10-02 00:01:20 +0800

    Add files via upload

--- api/api.py
+++ api/api.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request
-import pymongo
+#import pymongo
+from pymongo import MongoClient 
 app = Flask(__name__)
+
+myclient = pymongo.MongoClient("mongodb+srv://cluster0.wc9bxyx.mongodb.net/myFirstDatabase")
+mydb = myclient["Cluster0"]
+mydb.authenticate('admin','lhr368519')
+mycol = mydb["sites"]
  
+mydict = { "name": "RUNOOB", "alexa": "10000", "url": "https://www.runoob.com" }
 @app.route('/api/data/up_message/')
 def up_message():
-     return "111"
+    mycol.insert_one(mydict) 
+    return "true"
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
