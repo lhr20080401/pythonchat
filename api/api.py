89325eaebbc7aea76ba389b16bae34592af63bbe
lhr20080401 <112790672+lhr20080401@users.noreply.github.com>
2022-10-03 16:39:59 +0800

    g

--- api/api.py
+++ api/api.py
@@ -3,21 +3,11 @@ import pymongo
 #from pymongo import MongoClient 
 app = Flask(__name__)
 
-myclient = pymongo.MongoClient("mongodb+srv://cluster0.hirzqvu.mongodb.net/myFirstDatabase:27017",username="haor",password="lhr368519")
-#mongo_auth = myclient.admin
-mydb = myclient["Cluster0"]
-#mydb.authenticate('haor','lhr368519')
-#mydb = myclient["Cluster0"]
-mycol = mydb["sites"]
-lhr=1
-mydict = { "name": "RUNOOB", "alexa": "10000", "url": "https://www.runoob.com" }
+
 @app.route('/api/data/up_message/')
 def up_message():
     myclient = pymongo.MongoClient("mongodb+srv://cluster0.hirzqvu.mongodb.net/myFirstDatabase:27017",username="haor",password="lhr368519")
-#mongo_auth = myclient.admin
     mydb = myclient["Cluster0"]
-#mydb.authenticate('haor','lhr368519')
-#mydb = myclient["Cluster0"]
     mycol = mydb["sites"]
     lhr=1
     mydict = { "name": "RUNOOB", "alexa": "10000", "url": "https://www.runoob.com" }
